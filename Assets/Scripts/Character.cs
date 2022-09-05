cff78c53f89b5e1194456126576e0f23d83c0915
Olegsander78 <onkotov@gmail.com>
2022-09-05 16:49:23 +0500

    Сделано - подготовлен проект

--- Assets/Scripts/Character.cs
+++ Assets/Scripts/Character.cs
@@ -1,18 +1,34 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 
-public class Character : MonoBehaviour
+public abstract class Character : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    [Header("Stats")]
+    public int curHp;
+    public int maxHp;
 
-    // Update is called once per frame
-    void Update()
+    [Header("Components")]
+    public CharacterController Controller;
+    protected Character target;
+
+    public event UnityAction onTakeDamage;
+
+    public void TakeDamage(int damageToTake)
     {
-        
+        curHp -= damageToTake;
+        onTakeDamage?.Invoke();
+
+        if (curHp <= 0)
+            Die();
+    }
+    public void Die()
+    {
+        Destroy(gameObject);
+    }
+    public void SetTarget(Character t)
+    {
+        target = t;
     }
 }
--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -2,17 +2,12 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Player : MonoBehaviour
+public class Player : Character
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public static Player Current;
 
-    // Update is called once per frame
-    void Update()
+    private void Awake()
     {
-        
+        Current = this;
     }
 }
