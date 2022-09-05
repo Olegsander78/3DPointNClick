67df7fa65bf69deee184e2df13065d602b90ec2f
Olegsander78 <onkotov@gmail.com>
2022-09-05 17:07:09 +0500

    Сделано - Character controller

--- Assets/Scripts/CharacterController.cs
+++ Assets/Scripts/CharacterController.cs
@@ -1,18 +1,44 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class CharacterController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    private float moveToUpdateRate = 0.1f;
+    private float lastMoveToUpdate;
+    private Transform moveTarget;
 
-    // Update is called once per frame
-    void Update()
+    private NavMeshAgent agent;
+
+    private void Awake()
     {
-        
+        agent = GetComponent<NavMeshAgent>();
+    }
+    private void Update()
+    {
+        if (moveTarget != null && Time.time - lastMoveToUpdate < moveToUpdateRate)
+        {
+            lastMoveToUpdate = Time.time;
+            MoveToposition(moveTarget.position);
+        }
+    }
+    public void LookTowards(Vector3 direction)
+    {
+        transform.rotation = Quaternion.LookRotation(direction);
+    }
+    public void MoveToTarget(Transform target)
+    {
+        moveTarget = target;
+    }
+    public void MoveToposition(Vector3 position)
+    {
+        agent.isStopped = false;
+        agent.SetDestination(position);
+    }
+    public void StopMovement()
+    {
+        agent.isStopped = true;
+        moveTarget = null;
     }
 }
