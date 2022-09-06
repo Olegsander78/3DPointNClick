cb1e9ae425bdbc94e1d8c62dc66a2f0534c38f33
Olegsander78 <onkotov@gmail.com>
2022-09-06 14:07:35 +0500

    Сделано - healthbar

--- Assets/Scripts/HealthBarUI.cs
+++ Assets/Scripts/HealthBarUI.cs
@@ -1,18 +1,34 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
 
 public class HealthBarUI : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] private Image healthFill;
+    [SerializeField] private Character character;
+
+    private void OnEnable()
     {
-        
+        character.onTakeDamage += UpdateHealthBar;
+    }
+    private void OnDisable()
+    {
+        character.onTakeDamage -= UpdateHealthBar;
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Start()
     {
-        
+        UpdateHealthBar();
+    }
+    private void Update()
+    {
+        transform.rotation = Quaternion.LookRotation(transform.position - Camera.main.transform.position);
+    }
+
+    void UpdateHealthBar()
+    {
+        healthFill.fillAmount = (float)character.curHp / character.maxHp;
     }
 }
